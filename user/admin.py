787a268c193ef56a088dfdcd1475c4a6964a5edd
rafaelmoraiscunha <rafael.morais@franq.com.br>
2022-02-23 20:00:55 -0300

    Feat adiçao de model personalizada para usuario

---
+++ user/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import NewUser
+from django.contrib.auth.admin import UserAdmin
+
+
+class UserAdminConfig(UserAdmin):
+    ordering = ('-created_at',)
+    list_display = ('user_name', 'first_name', 'is_active', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('user_name', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('user_name', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+
+
+admin.site.register(NewUser, UserAdminConfig)
---
+++ user/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, user_name, first_name, last_name, password, **other_fields):
+
+        user_name = self.normalize_email(user_name)
+        user = self.model(user_name=user_name, first_name=first_name,
+                          last_name=last_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, user_name, first_name, last_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+        return self.create_user(user_name, first_name, last_name, password, **other_fields)
+
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
+    user_name = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=254)
+    last_name = models.CharField(max_length=254)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    objects = CustomAccountManager()
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.user_name
