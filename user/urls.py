1b0991ec1e0dd8ce0c6f2db64273a3898dfc8efb
rafaelmoraiscunha <rafael.morais@franq.com.br>
2022-02-26 12:16:24 -0300

    Feat cria view personalizada para authToken

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.authtoken import views
 from django.urls import path
 from user.views import UserView, UserDetailView
+from user.AuthTokenView import CustomAuthToken
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
+    path('api-token-auth/', CustomAuthToken.as_view()),
     path('', UserView.as_view()),
     path('<int:pk>', UserDetailView.as_view())
 ]
